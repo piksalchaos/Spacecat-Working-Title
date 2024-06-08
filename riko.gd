2dc252f01d77b138c1d72802658a007b7dced625
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-06-07 22:32:41 -0700

    Revert "character looks at mouse"

--- riko.gd
+++ riko.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 400.0
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
+
 	var x_direction = Input.get_axis("move_left", "move_right")
 	var y_direction = Input.get_axis('move_up', 'move_down')
 	
