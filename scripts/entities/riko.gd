228d8b6505bb6b4f1c06394c0518c667e98bda0f
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2024-07-27 18:18:53 -0700

    awawawa

--- scripts/entities/riko.gd
+++ scripts/entities/riko.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 @export var MELEE_COOLDOWN = 1
 @export var MELEE_ARC = 90 # in degrees?
 var projectile = preload("res://scenes/entities/projectile.tscn")
+
 func _physics_process(_delta):
 	#look_at(get_global_mouse_position())
 	var x_direction = Input.get_axis("move_left", "move_right")
@@ -16,14 +17,15 @@ func _physics_process(_delta):
 	
 	velocity = Vector2(x_direction, y_direction).normalized() * SPEED
 	#awvelocity.y /= 2
-	
 	move_and_slide()
+
 func fire_bullet(target):
 	var new_projectile = projectile.instantiate()
 	new_projectile.position = position
 	new_projectile.linear_velocity = (target - position).normalized() * BULLET_SPEED
 	new_projectile.look_at(target)
 	get_parent().add_child(new_projectile)
+
 func melee_attack():
 	var target_vector = (get_global_mouse_position() - position).normalized()
 	for enemy in $RikoMeleeAttack.get_overlapping_bodies():
@@ -31,6 +33,7 @@ func melee_attack():
 			continue
 		if enemy.has_node("HealthComponent"):
 			pass
+
 func _input(event):
 	if event.is_action_pressed("shoot"):
 		fire_bullet(get_global_mouse_position())
